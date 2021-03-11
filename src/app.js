0dac6c4a62248b9f55199a86d2f2f73b922c99e6
Stephanie Leazer <saleazer82@gmail.com>
2021-03-10 19:41:42 -0600

    Added functions

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const request = require('request')
+const chalk = require('chalk')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 //  Testing out what "__dirname" returns in the console
 console.log(__dirname)
@@ -42,11 +46,40 @@ app.get('/help', (req, res) => {
     })
 })
 app.get('/weather', (req, res) => {
-    res.send({
-        location: 'Edmond',
-        temperature: 77
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address.'
+        })
+    }
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+        if (error) {
+            return res.send({error})
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+            res.send({
+                forecast: {forecastData},
+                location,
+                address: req.query.address
+            })
+        })
     })
 })
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: "You must provide a search term..."
+        })
+    }
+    console.log(req.query.search)
+    res.send( {
+        products: []
+    })
+})
+
 app.get('/help/*', (req, res) => {
     res.render('pageNotFound', {
         title: "404 Page",
